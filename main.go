546f66a1dd85c3ac7d3e6be1095b432942da502d
saska <last.free.acc@gmail.com>
2017-05-01 13:55:45 +0300

    do go install for root (main.go)

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
 )
+
+const maingo = "main.go"
 
 func main() {
 	if len(os.Args) != 2 {
@@ -20,9 +23,27 @@ func main() {
 	dir, _ := filepath.Split(file)
 	pre := dir[:len(gosrc)]
 	if pre != gosrc {
-		log.Fatalf("[Error] Current package {%s} not in GOPATH {%s}\n", dir, gopath)
+		log.Fatalf("[Error] Current package {%s} is not in GOPATH {%s}\n", dir, gopath)
 	}
 	installPath := dir[len(gosrc)+1:]
+	tryFindMain := installPath
+OUTER:
+	for len(tryFindMain) > 1 {
+		fullPath := filepath.Join(gosrc, tryFindMain)
+		files, err := ioutil.ReadDir(fullPath)
+		if err != nil {
+			log.Printf("[Info] can not find %s, because of %s", maingo, err)
+			break
+		}
+		for _, f := range files {
+			if f.Name() == maingo {
+				installPath = tryFindMain
+				break OUTER
+			}
+		}
+
+		tryFindMain = filepath.Join(tryFindMain, "..")
+	}
 	cmd := exec.Command("go", "install", installPath)
 	if err := cmd.Run(); err != nil {
 		log.Fatalln(err)

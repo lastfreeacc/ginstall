830828c2235d6bfb3192d194eac763ac857a067b
saska <last.free.acc@gmail.com>
2017-05-01 18:53:31 +0300

    some refactor for pretty

--- main.go
+++ main.go
@@ -10,15 +10,12 @@ import (
 
 const maingo = "main.go"
 
+var (
+	gopath string
+	gosrc  string
+)
+
 func main() {
-	if len(os.Args) != 2 {
-		log.Fatalf("[Error] Wrong number of parametes: %v\n", os.Args)
-	}
-	gopath := os.Getenv("GOPATH")
-	if gopath == "" {
-		log.Fatalf("[Error] GOPATH is not set\n")
-	}
-	gosrc := filepath.Clean(gopath + "/src")
 	file := filepath.Clean(os.Args[1])
 	dir, _ := filepath.Split(file)
 	pre := dir[:len(gosrc)]
@@ -26,27 +23,46 @@ func main() {
 		log.Fatalf("[Error] Current package {%s} is not in GOPATH {%s}\n", dir, gopath)
 	}
 	installPath := dir[len(gosrc)+1:]
+	installPath = findMainDir(installPath)
+	cmd := exec.Command("go", "install", installPath)
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("[ERROR] can not run cmd 'go install %s', because of: %s\n", installPath, err)
+	}
+	log.Printf("[Info] Done: go install %s\n", installPath)
+}
+
+func init() {
+	// required
+	if len(os.Args) != 2 {
+		log.Fatalf("[Error] Wrong number of parametes: %v\n", os.Args)
+	}
+	// set global vars
+	gopath = os.Getenv("GOPATH")
+	if gopath == "" {
+		log.Fatalf("[Error] GOPATH is not set\n")
+	}
+	gosrc = filepath.Clean(gopath + "/src")
+}
+
+// findMainDir returns dir that contains main.go file
+// if it can not found then return input path
+func findMainDir(installPath string) string {
 	tryFindMain := installPath
-OUTER:
-	for len(tryFindMain) > 1 {
+	for len(tryFindMain) > 1 { // exit then tryFindMain = ".", TODO: need more strong condition
 		fullPath := filepath.Join(gosrc, tryFindMain)
 		files, err := ioutil.ReadDir(fullPath)
 		if err != nil {
-			log.Printf("[Info] can not find %s, because of %s", maingo, err)
+			log.Printf("[Info] can not find %s, because of: %s", maingo, err)
 			break
 		}
 		for _, f := range files {
 			if f.Name() == maingo {
-				installPath = tryFindMain
-				break OUTER
+				return tryFindMain // we find main.go
 			}
 		}
 
 		tryFindMain = filepath.Join(tryFindMain, "..")
 	}
-	cmd := exec.Command("go", "install", installPath)
-	if err := cmd.Run(); err != nil {
-		log.Fatalln(err)
-	}
-	log.Printf("[Info] Done: go install %s\n", installPath)
+	log.Printf("[Info] can not find %s\n", maingo)
+	return installPath // can not find main.go
 }
